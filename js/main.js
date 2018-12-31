617e25684d2e0085697a2e822ccf1981fc02e46d
Mohamed Gomaa <promgom3a@gmail.com>
2018-12-31 09:28:59 +0200

    Game without Canvas

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-let c = new Canvas("fish");
-c.create();
-
-let fish = new Fish(c.canvas.width / 2, c.canvas.height / 2, "./images/small_yellow_fish.gif",true);
-
-
 let score = 0;
 let level = 1;
 let foodTimer = 0;
@@ -15,50 +9,55 @@ let fishImagesPaths = ["./images/long_orange_fish.gif", "./images/dumy_fish.gif"
                        "./images/whale.gif", "./images/bubbles_fish.gif", 
                        "./images/yellow_fish.gif"];
 
-let background = new Image();
-background.src = "./images/land_ocean2.jpg";
+let fishPlayer = document.getElementById("fishPlayer");
+let container = document.getElementById("container");
 
+container.onmousemove = function(event){
 
+    let rect = container.getBoundingClientRect();
+    //console.log(rect.left + "  " + rect.top);
+    fishPlayer.style.left = (event.clientX - rect.left) + 'px';
+    fishPlayer.style.top = (event.clientY - rect.top) + 'px';
 
-window.onmousemove = function(event){
+    if(event.clientX >= window.innerWidth - fishPlayer.width ){
+        fishPlayer.style.left = window.innerWidth - fishPlayer.width + 'px';
+       // console.log(fishPlayer.style.left + "  Here " + fishPlayer.width );
+    }
 
-    if(fish.x < event.clientX)
-        fish.direction = 1; // right
-    else if(fish.x > event.clientX)
-        fish.direction = 0; // left
+    if(event.clientY >= window.innerHeight - fishPlayer.height ){
+        fishPlayer.style.top = window.innerHeight - fishPlayer.height + 'px';
+       // console.log(fishPlayer.style.top + "  Here " + fishPlayer.height );
+    }
     
-    let rect = c.canvas.getBoundingClientRect();
-    
-    fish.x = event.clientX - rect.left;
-    fish.y = event.clientY - rect.top;
-
 };
 
 
 function UpdateGameGrid() {
 
-    c.clear();
-    c.context.drawImage(background, 0, 0, c.canvas.width, c.canvas.height);
-    fish.drawFish(c.context);
 
-    foodTimer++;
-    if (foodTimer > 200 && fishList.length < 7) {
+
+    // c.clear();
+    // c.context.drawImage(background, 0, 0, c.canvas.width, c.canvas.height);
+    // fish.drawFish(c.context);
+
+    // foodTimer++;
+    // if (foodTimer > 200 && fishList.length < 7) {
        
-        fishList.push(new Fish(0,fishEntries[Math.round(Math.random() * 14)],
-                        fishImagesPaths[Math.round(Math.random() * 4)],false));
-        foodTimer = 0;
-    }
+    //     fishList.push(new Fish(0,fishEntries[Math.round(Math.random() * 14)],
+    //                     fishImagesPaths[Math.round(Math.random() * 4)],false));
+    //     foodTimer = 0;
+    // }
 
-    for(let i in fishList) {
-        if (fishList[i].x > c.canvas.width) { 
-            fishList.splice(i,1);
-        }
-        else {
-            fishList[i].x += fishList[i].speed;
-            fishList[i].drawFish(c.context);
-        }
+    // for(let i in fishList) {
+    //     if (fishList[i].x > c.canvas.width) { 
+    //         fishList.splice(i,1);
+    //     }
+    //     else {
+    //         fishList[i].x += fishList[i].speed;
+    //         fishList[i].drawFish(c.context);
+    //     }
 
-    }
+    // }
 
 
 }
