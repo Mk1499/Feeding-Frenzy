441d78332f0023110a2212e0b5a54d0562b66cc4
Mohamed Gomaa <promgom3a@gmail.com>
2019-01-02 10:55:28 +0200

    Change at the score and level counting function.

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ let gameover = false;
 let interval;
 let fishEnemiesList = [];
 let fishEntryPositions = [0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700];
-let fishImages = [{ src: "big_colored_fish.gif", weight: 1.7 },{ src: "gray_fish.gif", weight: 1.3 },{ src: "dumy_fish.gif", weight: 0.7 }, { src: "whale.gif", weight: 1.5 }];
+let fishImages = [{ src: "big_colored_fish.gif", weight: 1.7 },{ src: "gray_fish.gif", weight: 1.2 },{ src: "dumy_fish.gif", weight: 0.7 }, { src: "whale.gif", weight: 1.4 }];
 let playerNumber = 2;
 
 //localStorage.setItem("HScore" , 0) ; 
--- js/scoreAndLevel.js
+++ js/scoreAndLevel.js
@@ -1,28 +1,32 @@
-let scoreAndLevel = function(){
-    scoreRecord.innerHTML = score ; 
-    levelRecord.innerHTML = level ; 
-    HScoreRecord.innerHTML = localStorage.getItem("HScore") ; 
 
-    if (score === 0){
+let scoreAndLevel = function () {
 
-        fishPlayer.height = 60 ;
-        fishPlayer.width = 100 ; 
-    } 
-    else if (score === 10){
+    scoreRecord.innerHTML = score;
+    levelRecord.innerHTML = level;
+    HScoreRecord.innerHTML = localStorage.getItem("HScore");
 
-        fishPlayer.height = 100 ;
-        fishPlayer.width = 150 ; 
+    if (score === 0) {
+
+        fishPlayer.height = 60;
+        fishPlayer.width = 100;
     }
-    
-    if (score === 20 ){
-        fishPlayer.height = 150 ;
-        fishPlayer.width = 200 ; 
-    }
-    else if (score === 30){
-        level++ ; 
-        score = 0 ; 
+    else if (score === 10) {
+
+        fishPlayer.height = 60 * 1.3;
+        fishPlayer.width = 100 * 1.3;
     }
 
-    if ((score + level *10) > localStorage.getItem("HScore"))
-    localStorage.setItem("HScore",score) ; 
+    else if (score === 20) {
+
+        fishPlayer.height = 60 * 1.5;
+        fishPlayer.width = 100 * 1.5;
+    }
+    else if (score === 30) {
+
+        level++;
+        score = 0;
+    }
+
+    if ((score + (level * 10)) > localStorage.getItem("HScore"))
+        localStorage.setItem("HScore", score);
 }
