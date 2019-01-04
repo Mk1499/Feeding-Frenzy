95f5222813bdb9fd676b6a60bb722e9d328bd673
mk1499 <mkhaled1499@gmal.com>
2019-01-04 11:12:26 +0200

    openning

---
+++ js/load.js
@@ -0,0 +1,14 @@
+let lod=document.getElementById("load");
+
+let loaderOut = function () {
+  lod.classList.add("fadeOutUp") ; 
+}
+
+
+setTimeout( 
+
+  document.addEventListener("DOMContentLoaded", function(event) { 
+    loaderOut() ; 
+    })
+
+, 1000)  ; 
