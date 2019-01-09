959b23e0b042fe1ce0d1954db1ef3835e0af405d
Mohamed Gomaa <promgom3a@gmail.com>
2019-01-09 23:57:37 +0200

    Merge all changes done by us together.

--- js/finalBadges.js
+++ js/finalBadges.js
@@ -1,33 +1,31 @@
 
+let updateCurrentPlayerBadges = function () {
 
 
-let updateCurrentPlayerBadges=function(){
-
-
-let finalBadges=currentPlayerTempBadge;
-if (localStorage.getObj("highestScore").name==playerNa.value)
-   finalBadges.push(true);
-else
-   finalBadges.push(false);
-
-
-
-if (localStorage.getObj("highestNumLives").name==playerNa.value)
-   finalBadges.push(true);
-else
-   finalBadges.push(false);
-
-
-for (let i=0;i<finalBadges.length;i++){
-
-   if (finalBadges[i])
-    finalBadgesArr[i].style.display="block";
+   let finalBadges = currentPlayerTempBadge;
+   if (localStorage.getObj("highestScore").name == playerNa.value)
+      finalBadges.push(true);
    else
-    finalBadgesArr[i].style.display="none";
-}
+      finalBadges.push(false);
 
 
-console.log(finalBadges);
+
+   if (localStorage.getObj("highestNumLives").name == playerNa.value)
+      finalBadges.push(true);
+   else
+      finalBadges.push(false);
+
+
+   for (let i = 0; i < finalBadges.length; i++) {
+
+      if (finalBadges[i])
+         finalBadgesArr[i].style.display = "block";
+      else
+         finalBadgesArr[i].style.display = "none";
+   }
+
+
+   console.log(finalBadges);
 
 
 
